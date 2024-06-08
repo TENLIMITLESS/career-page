eb897c8f37415dbe25945eb589911dad933dd381
The Enterpreneurship Network <165583696+TENLIMITLESS@users.noreply.github.com>
2024-06-08 03:17:52 -0700

    Update InternshipCard.js
    content changed

--- src/components/InternshipCard.js
+++ src/components/InternshipCard.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
-const InternshipCard = ({ title, description,skills, email }) => {
+const InternshipCard = ({ title, description, skills, email }) => {
   return (
-    <div className="bg-white shadow-md rounded-lg p-6 m-4">
+    <div className="bg-white shadow-md rounded-lg p-6 m-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
       <h2 className="text-2xl font-bold mb-2">{title}</h2>
-      {/* <p className="text-gray-700 mb-4">{description}</p> */}
-      <ul>
-        <li> Location :Remote</li>
-        <li> Position Type: Internship</li>
-        <li> Duration : 3 months</li>
-        <li> Stipend : Unpaid</li>
+      <p className="text-gray-700 mb-4">{description}</p>
+      <ul className="text-gray-700 mb-4">
+        <li><strong>Location:</strong> Remote</li>
+        <li><strong>Position Type:</strong> Internship</li>
+        <li><strong>Duration:</strong> 3 months</li>
+        <li><strong>Compansation:</strong> Unpaid</li>
       </ul>
-      <p className=" text-gray-700 text-base"> Skills Required:{skills}</p>
-      <p>Please share your Resume on mail after click on apply button</p>
+      <p className="text-gray-700 text-base mb-4"><strong>Skills Required:</strong> {skills}</p>
+      <p className="text-gray-700 mb-4">Please share your resume via email after clicking on the apply button.</p>
       <a href={`mailto:${email}?subject=Application for ${title}`} className="inline-block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-200">
         Apply
       </a>
